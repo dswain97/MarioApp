eabe3d5401f1fa402bca0ca3d56f7e953fc259fb
Dustin <dswain4@my.wctc.edu>
2024-10-01 14:08:07 -0500

    1.07 Fixed with Nlog contents

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿﻿using NLog;
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
+
+// create instance of Logger
+var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+
+logger.Info("Program started");
+
+Console.WriteLine("Hello World!");
+
+
+string file = "mario.csv";
+// make sure movie file exists
+if (!File.Exists(file))
+{
+    logger.Error("File does not exist: {File}", file);
+}
+else
+{
+    string? choice;
+    do
+    {
+        // display choices to user
+        Console.WriteLine("1) Add Character");
+        Console.WriteLine("2) Display All Characters");
+        Console.WriteLine("Enter to quit");
+
+        // input selection
+        choice = Console.ReadLine();
+        logger.Info("User choice: {Choice}", choice);
+
+        if (choice == "1")
+        {
+            // Add Character
+        }
+        else if (choice == "2")
+        {
+            // Display All Characters
+        }
+    } while (choice == "1" || choice == "2");
+}
+
+   
+logger.Info("Program ended")
